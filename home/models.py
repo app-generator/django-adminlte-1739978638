f6af639c8a7c7801646924382878ae2008581e5e
App Generator <51070104+app-generator@users.noreply.github.com>
2025-02-19 17:24:09 +0200

    initial commit

---
+++ home/models.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Wireguard(models.Model):
+
+    #__Wireguard_FIELDS__
+    user = models.CharField(max_length=255, null=True, blank=True)
+    interface = models.CharField(max_length=255, null=True, blank=True)
+    server hostname = models.CharField(max_length=255, null=True, blank=True)
+    port = models.CharField(max_length=255, null=True, blank=True)
+    deactivate = models.BooleanField()
+
+    #__Wireguard_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Wireguard")
+        verbose_name_plural = _("Wireguard")
+
+
+
+#__MODELS__END
